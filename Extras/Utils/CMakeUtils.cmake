379c11fd80e1788131194581e7504ae399e7ae40
Kléber Kapelinski <kleberkapelinski@gmail.com>
2026-06-02 10:34:39 -0300

    [Extras] Centralizing assets directory

--- Extras/Utils/CMakeUtils.cmake
+++ Extras/Utils/CMakeUtils.cmake
@@ -1,10 +1,12 @@
 
+set(UTILS_DIR ${CMAKE_CURRENT_LIST_DIR})
+
 function(copy_assets_dir TARGET_NAME)
     add_custom_command(
     TARGET ${TARGET_NAME}
     POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E copy_directory
-    "${CMAKE_CURRENT_SOURCE_DIR}/assets"
+    "${UTILS_DIR}/assets"
     "$<TARGET_FILE_DIR:${TARGET_NAME}>/assets"
-    COMMENT "Copying: \"${CMAKE_CURRENT_SOURCE_DIR}/assets\" --> \"$<TARGET_FILE_DIR:${TARGET_NAME}>/assets\" ")
+    COMMENT "Copying: \"${UTILS_DIR}/assets\" --> \"$<TARGET_FILE_DIR:${TARGET_NAME}>/assets\" ")
 endfunction()
